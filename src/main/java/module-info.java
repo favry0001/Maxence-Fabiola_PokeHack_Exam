26473bd1d0c3adaf702dcbab5dc20bb49761ad55
favry <favry0001@gmail.com>
2026-07-10 18:28:20 -0400

    Ajout de l'interface PokemonDAO et son implémentation , correction du module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,7 @@
 module com.example.maxencefabiola_pokehack {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
 
     opens com.example.maxencefabiola_pokehack to javafx.fxml;
     exports com.example.maxencefabiola_pokehack;
