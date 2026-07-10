00558aaed732736358c4dc65b0afd7bd15ff8f23
Maxancedev <zkmaxance@gmail.com>
2026-07-10 12:17:22 -0400

    création de la structure du frontend

--- src/main/java/com/example/maxencefabiola_pokehack/controller/PokedexController.java
+++ src/main/java/com/example/maxencefabiola_pokehack/controller/PokedexController.java
@@ -1,4 +1,40 @@
 package com.example.maxencefabiola_pokehack.controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+
 public class PokedexController {
+
+    @FXML
+    private TextField searchField;
+
+    @FXML
+    private ImageView pokemonImage;
+
+    @FXML
+    private Label nameLabel;
+
+    @FXML
+    private Label idLabel;
+
+    @FXML
+    private Label typeLabel;
+
+    @FXML
+    private Label heightLabel;
+
+    @FXML
+    private Label weightLabel;
+
+    @FXML
+    private void searchPokemon() {
+
+        String recherche = searchField.getText();
+
+        System.out.println("Recherche : " + recherche);
+
+        // Le backend sera branché ici plus tard.
+    }
 }
