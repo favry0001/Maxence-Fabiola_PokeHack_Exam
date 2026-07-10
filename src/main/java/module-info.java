fdc4d2e1f2e56161fbb9fa8b9caa6f998d52a336
Maxancedev <zkmaxance@gmail.com>
2026-07-10 13:43:42 -0400

    Frontend JavaFX fonctionnel

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,8 @@ module com.example.maxencefabiola_pokehack {
     requires javafx.controls;
     requires javafx.fxml;
 
-
     opens com.example.maxencefabiola_pokehack to javafx.fxml;
+    opens com.example.maxencefabiola_pokehack.controller to javafx.fxml;
+
     exports com.example.maxencefabiola_pokehack;
 }
